4b31f4f06492def97dd57249193682c24744c95f
woobeen <armrit@naver.com>
2020-09-17 17:43:00 +0900

    2020-09-17 리뷰작성 및 저장 완료.

--- src/main/java/com/javaex/dao/ReviewDao.java
+++ src/main/java/com/javaex/dao/ReviewDao.java
@@ -1,5 +1,22 @@
 package com.javaex.dao;
 
-public class ReviewDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.javaex.vo.ReservationVo;
+import com.javaex.vo.ReviewVo;
+
+@Repository
+public class ReviewDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public ReservationVo tets(int userno) {
+		return sqlSession.selectOne("review.test",userno);
+	}
+	public int writereview(ReviewVo reviewvo) {
+		return sqlSession.insert("review.writeview", reviewvo);
+	}
 }
